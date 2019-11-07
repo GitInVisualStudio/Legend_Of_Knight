d50a6b13627fe98280bf7460bba8b44d2386b06c
Schüler-BG12 <Schler-BG12@JPRS-PC0086>
2019-11-07 10:05:51 +0100

    Erstellen der Klasse Game -> Basis für das Spiel

---
+++ Legend_Of_Knight/Game.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Legend_Of_Knight
+{
+    public class Game : Form
+    {
+        public const float FPS = 60.0f, TPS = 30.0f;
+        public const int WIDTH = 1280, HEIGHT = 720;
+        public const string NAME = "Legend of Knight";
+        private Timer renderTimer;
+
+        public Game()
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+            Name = NAME;
+            Width = WIDTH;
+            Height = HEIGHT;
+            //FormBorderStyle = FormBorderStyle.None; //TODO: Später Header selbst schreiben
+        }
+    }
+}
--- Legend_Of_Knight/Program.cs
+++ Legend_Of_Knight/Program.cs
@@ -16,7 +16,7 @@ namespace Legend_Of_Knight
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Game());
         }
     }
 }
