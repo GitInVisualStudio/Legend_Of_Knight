c112d680fd4f6f96c4290755a11f73576fbfbd7b
Jamin <yeates@web.de>
2019-11-12 16:07:57 +0100

    Sortierung von Utils

---
+++ Legend_Of_Knight/Utils/Math/CollisionArgs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Legend_Of_Knight.Entities;
+
+namespace Legend_Of_Knight.Utils.Math
+{
+    public class CollisionArgs
+    {
+        private BoundingBox[] boxes;
+
+        public BoundingBox[] Boxes
+        {
+            get
+            {
+                return boxes;
+            }
+
+            set
+            {
+                boxes = value;
+            }
+        }
+    }
+}
